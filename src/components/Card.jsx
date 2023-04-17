8cc5e8d3de926604248438b055790a415c346a92
Gabriel Diniz <gabriel.dinizdesouza@hotmail.com>
2023-04-17 17:10:31 -0300

    asdf

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,7 +9,7 @@ const Card = ({icon, show, name, text, myStyle, link}) => {
 
     return ( 
     <div className={Styles.containerCardSkills}>
-        <a className={Styles.cardSkills} onClick={clicar} style={myStyle} target="_blank">
+        <a className={Styles.cardSkills} onClick={clicar} style={myStyle} href={link} target="_blank">
             <div className={Styles.cardSkillsInside}>
                 {icon}
             </div>
